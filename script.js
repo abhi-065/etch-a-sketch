23dcc7bb0eef6f5ec6b7bd976e89fe200042a05b
D N S Abhiram <abhiramdns@gmail.com>
2025-07-04 06:18:59 +0000

    Added a button to pop up a prompt to enter the number of squares per side in grid

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const container = document.querySelector(".container");
-let n = 16;
-const length = 500/n;
-container.setAttribute("style", "display: flex; flex-direction: column; height: 500px; width: 500px; background-color: white;");
-for (let i=1; i<=n; i++){
-    let rowBox = document.createElement("div");
-    rowBox.setAttribute("style", `display: flex; flex-direction: row; height: ${length}px; width: 500px;`);
-    container.appendChild(rowBox);
-    for (let j=1; j<=n; j++){
-        let columnBox = document.createElement("div");
-        columnBox.setAttribute("style", `height: ${length}px; width: ${length}px;`);
-        columnBox.classList.add("box")
-        rowBox.appendChild(columnBox);
+const header = document.querySelector(".header");
+const popUpBtn = document.createElement("button");
+popUpBtn.textContent = "Click to enter number of squares";
+let noOfSquares;
+header.appendChild(popUpBtn);
+popUpBtn.addEventListener("click", () => {
+    noOfSquares = prompt("Enter number of squares per side:");
+    const length = 500/noOfSquares;
+    for (let i=1; i<=noOfSquares; i++){
+        let rowBox = document.createElement("div");
+        rowBox.setAttribute("style", `display: flex; flex-direction: row; height: ${length}px; width: 500px;`);
+        container.appendChild(rowBox);
+        for (let j=1; j<=noOfSquares; j++){
+            let columnBox = document.createElement("div");
+            columnBox.setAttribute("style", `height: ${length}px; width: ${length}px;`);
+            columnBox.classList.add("box");
+            rowBox.appendChild(columnBox);
+        }
     }
-}
+});
+container.setAttribute("style", "display: flex; flex-direction: column; height: 500px; width: 500px; background-color: white;");
+
