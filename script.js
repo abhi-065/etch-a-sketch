d41618a433b37673adb7de9fe62ab057a87b83cb
D N S Abhiram <abhiramdns@gmail.com>
2025-07-04 07:46:43 +0000

    Set a limit for the number of squares per side from 1 to 100

--- script.js
+++ script.js
@@ -14,26 +14,31 @@ let noOfSquares;
 header.appendChild(popUpBtn);
 popUpBtn.addEventListener("click", () => {
     noOfSquares = prompt("Enter number of squares per side:");
-    const length = 500/noOfSquares;
-    for (let i=1; i<=noOfSquares; i++){
-        const rowBox = document.createElement("div");
-        rowBox.setAttribute("style", `display: flex; flex-direction: row; height: ${length}px; width: 500px;`);
-        container.appendChild(rowBox);
-        for (let j=1; j<=noOfSquares; j++){
-            const columnBox = document.createElement("div");
-            columnBox.setAttribute("style", `height: ${length}px; width: ${length}px;`);
-            columnBox.classList.add("box");
-            columnBox.addEventListener("mouseover", () => {
-                const r = getRandomInt(0, 255);
-                const g = getRandomInt(0, 255);
-                const b = getRandomInt(0, 255);
-                columnBox.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-            });
-            rowBox.appendChild(columnBox);
+    if (noOfSquares>=1 && noOfSquares<=100){
+        const length = 500/noOfSquares;
+        for (let i=1; i<=noOfSquares; i++){
+            const rowBox = document.createElement("div");
+            rowBox.setAttribute("style", `display: flex; flex-direction: row; height: ${length}px; width: 500px;`);
+            container.appendChild(rowBox);
+            for (let j=1; j<=noOfSquares; j++){
+                const columnBox = document.createElement("div");
+                columnBox.setAttribute("style", `height: ${length}px; width: ${length}px;`);
+                columnBox.classList.add("box");
+                columnBox.addEventListener("mouseover", () => {
+                    const r = getRandomInt(0, 255);
+                    const g = getRandomInt(0, 255);
+                    const b = getRandomInt(0, 255);
+                    columnBox.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+                });
+                rowBox.appendChild(columnBox);
+            }
         }
+        popUpBtn.disabled = true;
+        header.appendChild(clearBtn);
     }
-    popUpBtn.disabled = true;
-    header.appendChild(clearBtn);
+    else {
+        alert("Number Should be between 1 and 100!");
+    }
 });
 clearBtn.addEventListener("click", () => {
     container.innerHTML = "";
