964daeb6d34099bf19440ddaa87551b8a5214d11
D N S Abhiram <abhiramdns@gmail.com>
2025-07-04 07:07:41 +0000

    Added the logic so that squares change their color to black on hover

--- script.js
+++ script.js
@@ -10,13 +10,14 @@ popUpBtn.addEventListener("click", () => {
     noOfSquares = prompt("Enter number of squares per side:");
     const length = 500/noOfSquares;
     for (let i=1; i<=noOfSquares; i++){
-        let rowBox = document.createElement("div");
+        const rowBox = document.createElement("div");
         rowBox.setAttribute("style", `display: flex; flex-direction: row; height: ${length}px; width: 500px;`);
         container.appendChild(rowBox);
         for (let j=1; j<=noOfSquares; j++){
-            let columnBox = document.createElement("div");
+            const columnBox = document.createElement("div");
             columnBox.setAttribute("style", `height: ${length}px; width: ${length}px;`);
             columnBox.classList.add("box");
+            columnBox.addEventListener("mouseover", () => {columnBox.style.backgroundColor = "black";});
             rowBox.appendChild(columnBox);
         }
     }
